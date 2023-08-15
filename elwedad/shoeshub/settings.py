307be590e1835ba5a9fac877e5f579759161b339
Mahmoud Gamal <mahmoudgamal19@gmail.com>
2023-08-15 23:10:10 +0300

    add static files

--- elwedad/shoeshub/settings.py
+++ elwedad/shoeshub/settings.py
@@ -150,7 +150,7 @@ STATIC_URL = '/static/static/'
 MEDIA_URL = '/static/media/'
 
 MEDIA_ROOT = '/vol/web/media/'
-STATIC_ROOT = '/vol/web/'
+STATIC_ROOT = '/vol/web/static/'
 # STATICFILES_DIRS = [
 #     'elwedad/static'
 # ]
